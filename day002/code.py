afa7ba002e576fc481c57f7ccfc0cf00a6cc286c
dyszernatalia <dyszernatalia@gmail.com>
2026-07-09 23:36:30 +0200

    Day 2 - Tip Calculator

--- day002/code.py
+++ day002/code.py
@@ -7,5 +7,6 @@ print(type(123.4)) #float
 
 print(type(True)) #bool
 
+#change
 # print("Numbers of letters in your name: " + len(input("Enter your name: ")))
 print("Numbers of letters in your name: " + str(len(input("Enter your name: "))))
