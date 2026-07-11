23c8effd8cec45b0793c82a4fa227271bc7642e5
dyszernatalia <dyszernatalia@gmail.com>
2026-07-12 00:58:57 +0200

    Day 3 - treasure island

--- day 003/treasure_island.py
+++ day 003/treasure_island.py
@@ -12,15 +12,15 @@ print('''
      ``-'               ``-'
 ''')
 
-left_or_right = input("Which way do you want to go? Left or Right? (L/R): ")
+left_or_right = input("Which way do you want to go? Left or Right? (left/right): ").lower()
 
-if left_or_right == "L":
-    lake = input("Good decision! But now there's a big lake in front of you. Do you want to swim or build a raft? (S/R): ")
+if left_or_right == "left":
+    lake = input("Good decision! But now there's a big lake in front of you. Do you want to swim or build a raft? (Swim/Raft): ").lower()
 
-    if lake == "R":
-        chest = input("You did it! In front of you are two chests. Which one do you choose? First or Second? (F/S): ")
+    if lake == "raft":
+        chest = input("You did it! In front of you are two chests. Which one do you choose? First or Second? (First/Second): ").lower()
 
-        if chest == "F":
+        if chest == "first":
             print("You picked the right one! Inside were the Scooby Snacks!!")
             print('''
     '------._.------'\\
@@ -41,7 +41,23 @@ Now Lucy is happy!!!!
             print("You picked the wrong one. Lucy is sad. Game Over.")
 
     else:
-        print("You picked the wrong one. Lucy is sad. Game Over.")
+        print("You made a wrong decision and got eat by a shark. Lucy is sad. Game Over.")
+        print('''
+                                 ,-
+                               ,'::|
+                              /::::|
+                            ,'::::o\                                      _..
+         ____........-------,..::?88b                                  ,-' /
+ _.--"""". . . .      .   .  .  .  ""`-._                           ,-' .;'
+<. - :::::o......  ...   . . .. . .  .  .""--._                  ,-'. .;'
+ `-._  ` `":`:`:`::||||:::::::::::::::::.:. .  ""--._ ,'|     ,-'.  .;'
+     """_=--       //'doo.. ````:`:`::::::::::.:.:.:. .`-`._-'.   .;'
+         ""--.__     P(       \               ` ``:`:``:::: .   .;'
+                "\""--.:-.     `.                             .:/
+                  \. /    `-._   `.""-----.,-..::(--"".\""`.  `:\
+                   `P         `-._ \          `-:\          `. `:\
+                                   ""            "            `-._)  
+''')
 
 else:
     print("You picked the wrong one. Lucy is sad. Game Over.")
